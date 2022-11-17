276344af6534a1bbc4f076250a9cf8f35ac246f0
yyj-02 <yeohyongjie@outlook.com>
2022-11-17 13:43:57 +0800

    Fix mobile view

--- src/components/organisms/ProjectCards/ProjectCards.tsx
+++ src/components/organisms/ProjectCards/ProjectCards.tsx
@@ -10,7 +10,7 @@ const ProjectCards: React.FC<props> = (props) => {
   return (
     <div className={`${style.parent} grid grid-cols-1 md:grid-cols-2`}>
       {props.projects.map((project) => (
-        <div className="m-5 flex hover:scale-105 transition-all duration-500">
+        <div className="m-5 flex md:hover:scale-105 transition-all duration-500">
           <ProjectCard {...project} />
         </div>
       ))}
