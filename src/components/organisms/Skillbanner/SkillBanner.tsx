f4d03e4184934c232aab3618c5612c61e18d09c6
yyj-02 <yeohyongjie@outlook.com>
2022-11-17 13:47:40 +0800

    Fix tooltip bug

--- src/components/organisms/Skillbanner/SkillBanner.tsx
+++ src/components/organisms/Skillbanner/SkillBanner.tsx
@@ -14,7 +14,7 @@ const ProjectCards: React.FC<props> = (props) => {
       </div>
       <div className="flex flex-row flex-wrap justify-center align-middle">
         {props.items.map((logo) => (
-          <div className="tooltip tooltip-bottom" data-tip={logo.name}>
+          <div className="tooltip tooltip-top" data-tip={logo.name}>
             <img
               src={logo.img}
               className="h-12 hover:scale-125 m-3 lg:h-20 w-auto lg:m-5 transition-all duration-500"
